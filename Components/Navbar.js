d3972c2b072aa676cb0b39f8e48ccce050a5dfab
119140173.hasiholan <89154087+blackhole2708@users.noreply.github.com>
2022-05-31 21:14:42 +0700

    Update Navbar.js

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 const Navbar = () => {
     return(
         <>
-        <View style={{backgroundColor:'green',height: 35}}>
-            <Text style={{color:'grey',margin:'auto'}}>Produk Kami</Text>
+        <View style={{backgroundColor:'black',height: 35}}>
+            <Text style={{color:'green',margin:'auto'}}>Produk Kami</Text>
         </View>
         </>
     )
